efdb7c4f5fd29cf2cb886438524d53d948cdfc0c
Roman <noteR@MacBook-Pro-Roman.local>
2017-11-05 21:20:36 +0200

    Simple example of Unity Ads (Rewarded Video) integration.

--- unityAdsTest/ViewController.swift
+++ unityAdsTest/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import UnityAds
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UnityAdsDelegate {
+    @IBOutlet weak var coinLbl: UILabel!
+    @IBOutlet weak var watchVideoBtn: UIButton!
+    
+    var coins: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //Init
+        UnityAds.initialize("1597356", delegate: self)
+        
+        watchVideoBtn.isHidden = true
+        coinLbl.text = String(coins)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //UnityAds implementation
+    func unityAdsReady(_ placementId: String) {
+        print("UnityAd Video is READY!")
+        watchVideoBtn.isHidden = false
     }
-
-
+    
+    func unityAdsDidStart(_ placementId: String) {
+        print("UnityAd Video is LAUNCHED!")
+        
+    }
+    
+    func unityAdsDidError(_ error: UnityAdsError, withMessage message: String) {
+        print("Error! UnityAd is FAILED!")
+    }
+    
+    func loadAndShowUnityAd() {
+        let placement = "rewardedVideo"
+        if (UnityAds.isReady(placement)) {
+            //a video is ready & placement is valid
+            UnityAds.show(self, placementId: placement)
+        }
+    }
+    
+    func unityAdsDidFinish(_ placementId: String, with state: UnityAdsFinishState) {
+        if (state != .skipped) {
+            //reward the player
+            coins += 5
+            coinLbl.text = String(coins)
+        }
+    }
+    @IBAction func watchVideoBtnPressed(_ sender: Any) {
+        loadAndShowUnityAd()
+        
+    }
+    
 }
 
